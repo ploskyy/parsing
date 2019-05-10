216e650191f34a7586770b79053287d1e8c04867
Ploskyy <ploskyy@gmail.com>
2019-05-10 11:38:26 +0300

    new views

--- parse/views.py
+++ parse/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    # flats = Flat.objects.all()
-    flats = [1,2,3]
+    flats = Flat.objects.all()
     # get_flats()
     return render(request, 'parse/index.html', {'flats': flats})
